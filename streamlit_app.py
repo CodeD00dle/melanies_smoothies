8592d08e4dcd942816476dd9f9d2ab845706d1d5
joeldreskin <163161972+joeldreskin@users.noreply.github.com>
2024-03-26 15:02:23 -0500

    Update streamlit_app.py
    Modify dataframe to pull from search_on column.

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,9 @@ session = Session.builder.configs(connection_parameters).create()
 
 #st.write('Session establilshed', session)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col("SEARCH_ON"))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients;', my_dataframe, max_selections = 5)
 
@@ -52,7 +53,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         #st.write(ingredients_string)
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
 
